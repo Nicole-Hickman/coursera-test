4a828106206362e269d7a8fdc18ee739eb6a4e3d
Nicole Hickman <nicole.hickman@fishawack.com>
2022-12-04 15:09:58 -0800

    module 4 test start

--- mod4-assignment/SpeakHello.js
+++ mod4-assignment/SpeakHello.js
@@ -1,14 +1,20 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
+(function (window) {
+
 
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var helloSpeaker =
+var helloSpeaker = {};
+window.helloSpeaker = helloSpeaker;
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
+helloSpeaker.speakHello = function () {
+  console.log(speakWord + helloSpeaker);
+}
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
@@ -22,3 +28,6 @@ function speak(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+
+
+}) (window);
